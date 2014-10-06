71e4704620884416d13d40d94e77bd0fc337f1f5
Stuart P. Bentley <stuart@testtrack4.com>
2014-10-05 21:44:51 -0700

    Initial content ported from hashblot.js

--- README.md
+++ README.md
@@ -1,4 +1,71 @@
-any_sha1
-========
+# any_sha1
 
-JavaScript functions for abstracting SHA1 implementations
+This package provides JavaScript functions for abstracting SHA-1
+implementations, providing a uniform signature and interface.
+
+When `module` is defined, this function will require and use the `crypto`
+module to produce the SHA-1 hash.
+
+Otherwise, it will detect and use SHA-1 functions from the following projects,
+in order:
+
+- `require("crypto")`, for NodeJS / browserify (based on the presence of
+  `module`)
+- [Rusha](https://github.com/srijs/rusha)
+- [Forge](https://github.com/digitalbazaar/forge#sha1)
+- [SJCL](https://github.com/bitwiseshiftleft/sjcl)
+- [jsSHA](https://github.com/Caligatio/jsSHA)
+- [Paul Johnston's hex_sha1 function](http://pajhome.org.uk/crypt/md5/)
+- [CryptoJS](https://code.google.com/p/crypto-js/)
+- [PolyCrypt](https://github.com/polycrypt/polycrypt)
+
+(Pull requests to add more implementations are welcome.)
+
+## any_sha1.from(opts)
+
+Detects for the presence of the projects listed above, and returns the first
+function from `opts` that matches a detected environment (or `null`, if no
+recognized SHA-1 implementation was found).
+
+The "opts" parameter is expected to come from one of the sets defined below,
+where the first level is the input encoding to hash (eg. 'utf8') and the
+second level is the output type (eg. 'bytes' for an array of byte values).
+
+## any_sha1.define(opts,[obj,[key,[chooser]]])
+
+Defines `key` (by default, `sha1`) in `obj` (by default, `window`) to the
+function in `opts` selected by `chooser` (by default, `any_sha1.from`).
+
+If no appropriate SHA-1 function is found (`chooser(opts)` returns `null`),
+this function will return / define a function that will attept to define the
+SHA-1 function when first called, allowing for a late-binding approach where
+the appropriate SHA-1 implementation is introduced after this function is
+invoked.
+
+## Input encodings: any_sha1.utf8
+
+Output options under this encoding type treat extended characters in the given
+string outside the ASCII set as their UTF-8 byte representations. (Due to the
+modern ubiquity of the UTF-8 encoding, nowadays, this is usually what you
+want: as such, for the moment, this is the only input encoding this package
+provides.)
+
+For SHA-1 functions that don't handle strings with character codes outside the
+0-255 range (pretty much all of them), the string will first be converted to
+UTF-8 via the [unescape-encodeURIComponent method][1].
+
+[1]: http://ecmanaut.blogspot.com/2006/07/encoding-decoding-utf8-in-javascript.html
+
+## Output options: any_sha1.utf8.bytes
+
+Returns the SHA-1 hash as an array of byte values.
+
+## Example usage
+
+To define a default SHA-1 function for [hashblot][], allowing for late binding:
+
+```
+any_sha1.define(any_sha1.utf8.bytes, hashblot);
+```
+
+[hashblot]: https://github.com/stuartpb/hashblot
---
+++ any_sha1.js
@@ -0,0 +1,145 @@
+/*global Rusha hex_sha1 jsSHA sjcl forge CryptoJS*/
+
+/*any_sha1 Copyright (c) 2014 Stuart P. Bentley
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+(function(){
+
+"use strict";
+
+var crypto, rusha;
+
+function charCodes(s) {
+  var l = s.length;
+  var a = new Array(l);
+  for (var i = 0; i < l; ++i) {
+    a[i] = s.charCodeAt(i);
+  }
+  return a;
+}
+
+function hexBytes(s) {
+  var l = s.length / 2;
+  var a = new Array(l);
+  for (var i = 0; i < l; ++i) {
+    a[i] = parseInt(s.slice(i*2, 2), 16);
+  }
+  return a;
+}
+
+function from(opts) {
+  if (typeof module != "undefined") {
+    crypto = crypto || require('crypto');
+    return opts.node;
+  } else if (typeof(Rusha) != "undefined") {
+    rusha = rusha || new Rusha();
+    return opts.rusha;
+  } else if (typeof(forge) != "undefined") {
+    return opts.forge;
+  } else if (typeof(sjcl) != "undefined" && sjcl.hash.sha1) {
+    return opts.sjcl;
+  } else if (typeof(jsSHA) != "undefined") {
+    return opts.jsSHA;
+  } else if (typeof(hex_sha1) != "undefined") {
+    return opts.paj;
+  } else if (typeof(CryptoJS) != "undefined") {
+    return opts.cryptoJS;
+  } else if (window.polycrypt) {
+    return opts.polycrypt;
+  } else return null;
+}
+
+var any_sha1 = { utf8: {
+  bytes: {
+    node: function sha1Node(content) {
+      crypto = crypto || require('crypto');
+      return Array.apply([],
+        crypto.createHash('sha1').update(content, 'utf8').digest());
+    },
+    rusha: function sha1Rusha(content) {
+      rusha = rusha || new Rusha();
+      return Array.apply([], new Uint8Array(
+        rusha.rawDigest(unescape(encodeURIComponent(content))).buffer));
+    },
+    forge: function sha1Forge(content) {
+      return charCodes(forge.md.sha1.create()
+        .update(unescape(encodeURIComponent(content))).digest().bytes());
+    },
+    sjcl: function sha1Sjcl(content) {
+      var result = sjcl.hash.sha1.hash(unescape(encodeURIComponent(content)));
+      var a = new Array(20);
+      for (var i = 0; i < 5; ++i) {
+        a[i*4] = result[i] & 0xFF;
+        a[i*4+1] = result[i] >> 8 & 0xFF;
+        a[i*4+2] = result[i] >> 16 & 0xFF;
+        a[i*4+3] = result[i] >> 24;
+      }
+      return a;
+    },
+    jsSHA: function sha1JsSHA(content) {
+      return hexBytes(new jsSHA(unescape(encodeURIComponent(content)),'TEXT')
+        .getHash('SHA-1','HEX'));
+    },
+    paj: function sha1Paj(content) {
+      return hexBytes(hex_sha1(content));
+    },
+    cryptoJS: function sha1CryptoJS(content) {
+        return hexBytes(CryptoJS.SHA1(unescape(encodeURIComponent(content))));
+    },
+    polycrypt: function sha1Polycrypt(content) {
+      return hexBytes(window.polycrypt.digest('SHA-1',
+        unescape(encodeURIComponent(content))));
+    }
+  }},
+
+  define: function define(opts, obj, key, chooser) {
+    obj = obj || window;
+    key = key || 'sha1';
+    chooser = chooser || from;
+    if (typeof(Rusha) != "undefined")
+      rusha = new Rusha();
+
+    if (obj[key]) {
+      return obj[key];
+    }
+
+    var f = chooser(opts);
+    if (f) {
+      return obj[key] = f;
+    } else {
+      return obj[key] = function sha1LateBinding(content) {
+        var f = chooser(opts);
+        obj[key] = f;
+        return f(content);
+      };
+    }
+  },
+
+  from: from, charCodes: charCodes, hexBytes: hexBytes
+};
+
+if(typeof module != "undefined") {
+  module.exports = any_sha1;
+  crypto = require('crypto');
+} else {
+  window.any_sha1 = any_sha1;
+}
+})();
